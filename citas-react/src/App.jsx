fef2cbfc4df00f13c1e955630179055ce0e1f146
Davidlamilla <jdlamilla2019-2@corhuila.edu.co>
2023-01-12 20:02:02 -0500

    Inicio del proyecto

---
+++ citas-react/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  /* estado de retorno constante */
+
+  return (
+    /* etiqueta padre y el body de la pagina */
+    <div>
+    </div>
+  )
+}
+/* Exportar para importar el modulo a otras partes de body */
+export default App
