dd689d00fea4741a0ec118b7a40a8c1a81d34cc6
Davidlamilla <jdlamilla2019-2@corhuila.edu.co>
2023-01-13 20:15:40 -0500

    Actualiacion

--- citas-react/src/assets/Componets/ListaPacientes.jsx
+++ citas-react/src/assets/Componets/ListaPacientes.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const ListaPacientes = () => {
   return (
-    <>
-    <div>ListaPacientes</div>
-    </>
+   <div className="w-1/2 lg:w-3/5">
+    <h2>Listado Pacientes</h2>
+   </div>
     
   )
 }
