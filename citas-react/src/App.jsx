3dc2c900acd80ecd097df1d1cbaa227b4033026e
Davidlamilla <jdlamilla2019-2@corhuila.edu.co>
2023-01-13 20:09:51 -0500

    Avance clase 13/01/2023

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -4,20 +4,21 @@ import { Formulario } from "./assets/Componets/Formulario"
 import Header from "./assets/Componets/Header"
 import { ListaPacientes } from "./assets/Componets/ListaPacientes"
 
-/* Funcion creadora de la aplicacion */
 function App() {
 
   return (
    
-    /* etiqueta padre y el body de la pagina */
-    <div>
+    <div className="container mx-auto mt-20">
 
     <Header/>
+    <div className="mt-12 md:flex">
     <Formulario/>
     <ListaPacientes/>
+    </div>
+  
 
     </div>
   )
 }
-/* Exportar para importar el modulo a otras partes de body */
+
 export default App
--- citas-react/src/assets/Componets/Formulario.jsx
+++ citas-react/src/assets/Componets/Formulario.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export const Formulario = () => {
   return (
-    <>
-    <div>Formulario</div>
-    </>
+   <div className="md:w-1/2 lg:w-2/5">
+    <h2 className="font-black text-3xl text-center">
+      Seguimiento pasientes
+    </h2>
+    <p className='text-lg mt-5 text-center'>
+      Añade pacientes {" "}
+    </p>
+      <span className="text-indigo-600 font-bold">Administralos</span>
+      
+        <form action="">
+
+        </form>
+   </div>
   )
 }
