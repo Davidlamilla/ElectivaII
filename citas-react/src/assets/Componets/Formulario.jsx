abe4c4de2f5f82389a0eb3c2e6b3f282c35b54c7
Davidlamilla <jdlamilla2019-2@corhuila.edu.co>
2023-01-16 19:29:23 -0500

    Avance clase 16-01-23

--- citas-react/src/assets/Componets/Formulario.jsx
+++ citas-react/src/assets/Componets/Formulario.jsx
@@ -3,17 +3,26 @@ import React from 'react'
 export const Formulario = () => {
   return (
    <div className="md:w-1/2 lg:w-2/5">
-    <h2 className="font-black text-3xl text-center">
-      Seguimiento pasientes
+    <h2 className="font-black text-3xl text-center mb-10">
+      Seguimiento de pasientes
     </h2>
-    <p className='text-lg mt-5 text-center'>
+    <p className="text-lg mt-5 text-center"> 
       Añade pacientes {" "}
+      <span className='text-indigo-600'
+      font-bold>Administralos</span>
+     
     </p>
-      <span className="text-indigo-600 font-bold">Administralos</span>
-      
-        <form action="">
 
-        </form>
+    <form className='bg-white shadow-md rounded-lg py-10 px-5'>
+      <div className='mb-5'>
+        <label htmlFor="mascota" className="block
+          text-gray-700 uppercase font-bold">Nombre Mascota:</label>
+
+        <input id="mascota" type="text" className='border-2
+        w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+        placeholder='Nombre de la mascota' />
+      </div>
+    </form>  
    </div>
   )
 }
