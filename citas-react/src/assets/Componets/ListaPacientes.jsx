37d734a7d7933158022f7ea0295a5b86d269c706
Davidlamilla <jdlamilla2019-2@corhuila.edu.co>
2023-01-17 19:38:21 -0500

    clase 17

--- citas-react/src/assets/Componets/ListaPacientes.jsx
+++ citas-react/src/assets/Componets/ListaPacientes.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Pacientes from "./Pacientes"
+
+Pacientes
 
 export const ListaPacientes = () => {
   return (
-   <div className="w-1/2 lg:w-3/5">
-    <h2>Listado Pacientes</h2>
+   <div className="md:w-1/2 lg:w-3/5 h-screen overflow-y-scroll">
+    <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
+    <p className='text-xl mt-5 mb-10 text-center'>
+      Administrar tus {' '}
+      <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+    </p>
+
+    <Pacientes/>
+    <Pacientes/>
+    <Pacientes/>
+    <Pacientes/>
+    
    </div>
     
   )
---
+++ citas-react/src/assets/Componets/Pacientes.jsx
@@ -0,0 +1,31 @@
+
+
+function Pacientes() {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre:{' '} <span className='font-normal normal-case'>Trosky</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario:{' '} <span className='font-normal normal-case'>Juan David Lamilla</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email:{' '} <span className='font-normal normal-case'>Juandavidlamilla89@gmail.com</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha de Ingreso:{' '} <span className='font-normal normal-case'>17 de Enero de 2023</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Sintomas:{' '} <span className='font-normal normal-case'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Veritatis sint ut aut numquam aspernatur voluptas. Corrupti deserunt fuga itaque dolorem laborum cupiditate, impedit 
+         ipsum repudiandae nulla necessitatibus aspernatur sequi saepe!</span>
+      </p>
+    </div>
+  )
+}
+
+export default Pacientes
