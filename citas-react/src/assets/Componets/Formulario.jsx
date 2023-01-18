0deeada33cc0191f6305073718bad0fd1c24da49
Davidlamilla <jdlamilla2019-2@corhuila.edu.co>
2023-01-17 20:15:22 -0500

    Actu

--- citas-react/src/assets/Componets/Formulario.jsx
+++ citas-react/src/assets/Componets/Formulario.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { useState,userEffect  } from "react"
 
 export const Formulario = () => {
-  return (
+  const [nombre, setNombre] = useState('Trosky');
+
+  setNombre(´Trosky)
+  console.log(nombre);
+   return (
    <div className="md:w-1/2 lg:w-2/5">
     <h2 className="font-black text-3xl text-center mb-10">
       Seguimiento de pasientes
