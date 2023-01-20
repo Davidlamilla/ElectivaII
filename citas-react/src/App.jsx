4715a7ea6fc505d6516266e22c3660be9f244bf6
Davidlamilla <jdlamilla2019-2@corhuila.edu.co>
2023-01-19 20:14:41 -0500

    clase 19-1-23

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -15,8 +15,6 @@ function App() {
     <Formulario/>
     <ListaPacientes/>
     </div>
-  
-
     </div>
   )
 }
--- citas-react/src/assets/Componets/Formulario.jsx
+++ citas-react/src/assets/Componets/Formulario.jsx
@@ -1,34 +1,113 @@
-import { useState,userEffect  } from "react"
+import { useState, useEffect } from "react";
 
-export const Formulario = () => {
-  const [nombre, setNombre] = useState('Trosky');
+const Formulario = () => {
+  cont [nombre, setNombre] = useState(" ");
+  cont [propietarios, setPropietario] = useState(" ");
+  cont [email, setEmail] = useState(" ");
+  cont [fingreso, setPFingreso] = useState(" ");
+  cont [sintomas, setSintomas] = useState(" ");
 
-  setNombre(´Trosky)
-  console.log(nombre);
-   return (
-   <div className="md:w-1/2 lg:w-2/5">
-    <h2 className="font-black text-3xl text-center mb-10">
-      Seguimiento de pasientes
-    </h2>
-    <p className="text-lg mt-5 text-center"> 
-      Añade pacientes {" "}
-      <span className='text-indigo-600'
-      font-bold>Administralos</span>
-     
-    </p>
+  //hook para capturar errores
 
-    <form className='bg-white shadow-md rounded-lg py-10 px-5'>
-      <div className='mb-5'>
-        <label htmlFor="mascota" className="block
-          text-gray-700 uppercase font-bold">Nombre Mascota:</label>
+  cont[error,setError]= useState(false);
 
-        <input id="mascota" type="text" className='border-2
-        w-full p-2 mt-2 placeholder-gray-400 rounded-md'
-        placeholder='Nombre de la mascota' />
+  //Limpiar o resetear el formulario cuando carga la pagina
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if([nombre,propietarios,email,fingreso,sintomas].includes
+      ("")){
+        
+        setError(true);
+        return;
+
+      }
+
+      setError(false);
+      
+      }
+  
+  }
+
+  // Validar formulario que no vaya con campos vacios
+ 
+  return (
+
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
+        <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+      <p className="text-lg mt-5 text-center mb-10">
+        Añade Pacientes y {' '}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10" onSubmit={handleSubmit}>
+        {error && (<div className="bg-red-800 text-white p-3
+        uppercase text-center font-bold rounded-md">
+          <p>
+            Todos los campos son Obligatorios!
+          </p>
+          </div>)
+          }
+
+        <div className="mb-5">
+
+          <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
+            Nombre Mascota:
+          </label>
+          <input id="mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+          type="text" placeholder="Nombre de la mascota" value={nombre} onChange={(e)(e.target.value)}/>
+          
+        </div>
+
+
+        <div className="mb-5">
+          
+          <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">
+            Nombre Propietario:
+          </label>
+          <input id="propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+          type="text" placeholder="Nombre del propietario"></input> 
+        </div>
+
+
+        <div className="mb-5">
+          
+          <label htmlFor="email" className="block text-gray-700 uppercase font-bold">
+            Email Propietario:
+          </label>
+          <input id="email" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+          type="email" placeholder="Email del propietario"></input>
+        </div>
+
+
+        <div className="mb-5">
+          
+          <label htmlFor="fecha" className="block text-gray-700 uppercase font-bold">
+            Fecha de Ingreso:
+          </label>
+          <input id="fecha" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+          type="date"/>
+          
+        </div>
+
+
+        <div className="mb-5">
+          
+          <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">
+            Síntomas:
+          </label>
+          <textarea className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+          id="sintomas"  placeholder="Describe los síntomas" />
+          
+        </div>
+
+        <input type="submit" className="bg-indigo-600 w-full p-3
+         text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors" 
+         value="Agregar Paciente"/>
+        
+      </form>
     
-
-      </div>
-    </form>  
-   </div>
+    </div>
   )
 }
+
+export default Formulario;
